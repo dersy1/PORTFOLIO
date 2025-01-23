e9b35cc9bfcddea24342cd1b5249baa8188e7e16
Pablo <pablot.pavan@gmail.com>
2025-01-23 18:37:56 -0300

    added projects button

--- js/toggle.js
+++ js/toggle.js
@@ -1,31 +1,38 @@
-const button=document.getElementById('eng');
-const subtitle=document.getElementById('subtitle')
-const about=document.getElementById('about-me')
-const description=document.getElementById('description');
-const download=document.getElementById('download');
+const button = document.getElementById('eng');
+const subtitle = document.getElementById('subtitle');
+const about = document.getElementById('about-me');
+const description = document.getElementById('description');
+const download = document.getElementById('download');
+const folderText = document.getElementById('folder-text');
+const folder = document.getElementById('folder');
 let es = true;
-subtitle.textContent= 'Desarrollador web';
-about.textContent='Sobre mí';
-description.textContent= 'Soy un desarrollador web full stack. Disfruto trabajando en equipo. Poseo formación avanzada en dibujo, animación e inglés.';
-download.textContent='VER CV';
+
+subtitle.textContent = 'Desarrollador web';
+about.textContent = 'Sobre mí';
+description.textContent = 'Soy un desarrollador web full stack. Disfruto trabajando en equipo. Poseo formación avanzada en dibujo, animación e inglés.';
+download.textContent = 'VER CV';
+folderText.textContent = 'Proyectos';
+
 button.addEventListener('click', function() {
     es = !es;
-    if(es){
-    button.textContent = 'ES';
-    subtitle.textContent= 'Web developer';
-    about.textContent= 'About me';
-    description.textContent='I am a full-stack web developer, who enjoys working in a team. I have advanced training in English, drawing and animation.';
-    download.textContent='MY RESUME';
-    }else{  
-    button.textContent = 'ENG';
-    subtitle.textContent= 'Desarrollador web';
-    about.textContent='Sobre mi';
-    description.textContent='Actualmente estudio Desarrollo Web en la UnLaM. Soy una persona responsable'+
-    'predispuesta a adquirir nuevos conocimientos y me gusta trabajar en equipo.'+ 
-    'Poseo una formación avanzada de inglés y animación.';
-    download.textContent='VER CV';
+    if (es) {
+        button.textContent = 'ES';
+        subtitle.textContent = 'Web developer';
+        about.textContent = 'About me';
+        description.textContent = 'I am a full-stack web developer, who enjoys working in a team. I have advanced training in English, drawing, and animation.';
+        download.textContent = 'MY RESUME';
+        folderText.textContent = 'Projects';
+        folder.querySelector('i').textContent = 'folder';
+    } else {
+        button.textContent = 'ENG';
+        subtitle.textContent = 'Desarrollador web';
+        about.textContent = 'Sobre mí';
+        description.textContent = 'Soy un desarrollador web full stack. Disfruto trabajando en equipo. Poseo formación avanzada en dibujo, animación e inglés.';
+        download.textContent = 'VER CV';
+        folderText.textContent = 'Proyectos';
+        folder.querySelector('i').textContent = 'folder';
     }
-  });
+});
 
 
 
