2c25d728e811513661ef495705b96842ae4cbe47
dersy1 <pablot.pavan@gmail.com>
2025-02-11 19:40:32 -0300

    improved style of cards

--- obliqueStrategies.js
+++ obliqueStrategies.js
@@ -1,10 +1,10 @@
 const cardText =document.getElementById("cardText");
 const cards = new Map();
-cards.set(1, "HOLA");
-cards.set(2, "CHAU");
+cards.set(1, "Remove specifics and convert to ambiguities");
+cards.set(2, "Think of the radio");
 
 function showRandomCard(cards){
-    
+
     const keys = Array.from(cards.keys()); 
     const randomKey = keys[Math.floor(Math.random() * keys.length)]; 
     cardText.innerHTML = cards.get(randomKey);  
